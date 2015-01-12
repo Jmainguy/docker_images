df4abbdd2cd3eacb0853c8790d0d78b12f336052
Jonathan Mainguy <jon@soh.re>
2015-01-11 20:55:31 -0500

    Docker image for prosody jabber server

---
+++ prosody/prosody/share/lua/5.1/luarocks/site_config.lua
@@ -0,0 +1,13 @@
+local site_config = {}
+site_config.LUAROCKS_PREFIX=[[/opt/prosody]]
+site_config.LUA_INCDIR=[[/usr/include]]
+site_config.LUA_LIBDIR=[[/usr/lib]]
+site_config.LUA_BINDIR=[[/usr/bin]]
+site_config.LUAROCKS_SYSCONFDIR=[[/opt/prosody/etc/luarocks]]
+site_config.LUAROCKS_ROCKS_TREE=[[/opt/prosody]]
+site_config.LUAROCKS_ROCKS_SUBDIR=[[/lib/luarocks/rocks]]
+site_config.LUAROCKS_UNAME_S=[[Linux]]
+site_config.LUAROCKS_UNAME_M=[[x86_64]]
+site_config.LUAROCKS_DOWNLOADER=[[curl]]
+site_config.LUAROCKS_MD5CHECKER=[[md5sum]]
+return site_config
