69b78688f1b0da60aaec26bb7e15114c07c95620
Jonathan Mainguy <jon@soh.re>
2016-10-15 18:03:55 -0400

    fix typos

--- etherpad/run.sh
+++ etherpad/run.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 # This is to daemonize the container on the host
-docker run -d -p 8080:9001 \
+docker run -d -p 9001:9001 \
 -e MYSQL_USER=jar \
 -e MYSQL_PASSWORD=jar \
 -e MYSQL_DATABASE=etherpad \
 -e MYSQL_HOST=binks.soh.re
 -e ADMIN_USER=admin \
 -e ADMIN_PASSWORD=password \
---restart=always \
+--restart always \
 --name etherpad \
 soh.re/etherpad
